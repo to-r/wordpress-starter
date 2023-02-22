12ce62e4f85c677a48dfdb1e80f895138698339a
Katsushi OUGI <ougi@to-r.net>
2023-02-22 19:15:53 +0900

    eslintにブラウザの設定を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,8 @@
 module.exports = {
-  parser: "@babel/eslint-parser",
   env: {
     browser: true,
     es6: true,
-    node: true,
   },
-  extends: ["eslint:recommended", "prettier"],
-  parserOptions: {
-    sourceType: "module",
-  },
+  plugins: ["compat"],
+  extends: ["plugin:compat/recommended", "prettier"],
 };
