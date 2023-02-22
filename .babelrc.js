5bba0688d83b57a97e6e0a401d1f3835f240f307
Katsushi OUGI <ougi@to-r.net>
2023-02-22 18:44:06 +0900

    eslint設定

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,1 @@
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        useBuiltIns: "usage",
-        corejs: 3,
-      },
-    ],
-  ],
-  plugins: ["@babel/plugin-transform-runtime"],
-};
+module.exports = {};
