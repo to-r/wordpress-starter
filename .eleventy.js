2e5ee15353216b4b3eee4430fa1c514859ecd4ff
Katsushi OUGI <ougi@to-r.net>
2023-05-16 16:33:22 +0900

    シンプルな構成に変更

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const path = require("path");
 
+const THEME_PATH = "wp-content/themes/my-theme";
+
 module.exports = (config) => {
-  const manifestPath = path.resolve(__dirname, "manifest.json");
+  // const manifestPath = path.resolve(__dirname, "manifest.json");
 
   // nunjucks settings
   config.setNunjucksEnvironmentOptions({
@@ -9,17 +11,27 @@ module.exports = (config) => {
     autoescape: false, // warning: don’t do this!
   });
 
+  config.addPassthroughCopy(path.join(THEME_PATH, "static/assets/js"));
+  config.addPassthroughCopy(path.join(THEME_PATH, "static/assets/img"));
+  config.addPassthroughCopy(path.join(THEME_PATH, "static/assets/css"));
+
+  config.addWatchTarget(path.join(THEME_PATH, "static/assets/scss"));
+
   // BrowserSync Overrides
   config.setBrowserSyncConfig({
     ...config.browserSyncConfig,
-    // Reload when manifest file changes
-    files: [manifestPath],
     // Speed/clean up build time
     ui: false,
     ghostMode: false,
+    open: "localhost",
+    startPath: "/templates",
   });
 
   return {
-    dir: { input: "src/njk", output: "site", includes: "_includes" },
+    dir: {
+      input: path.join(THEME_PATH, "static"),
+      output: path.join(THEME_PATH, "html-template"),
+      includes: "_includes",
+    },
   };
 };
