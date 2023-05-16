fe805503517ddf35fc576ef96d4c47229aebe7b9
Katsushi OUGI <ougi@to-r.net>
2023-05-16 16:36:32 +0900

    function修正

--- wp-content/themes/my-theme/functions.php
+++ wp-content/themes/my-theme/functions.php
@@ -3,7 +3,7 @@
 add_action(
 	'wp_enqueue_scripts',
 	function () {
-		wp_enqueue_style('main', get_theme_file_uri('assets/main.css'));
-		wp_enqueue_script('main', get_theme_file_uri('assets/main.js'), [], '', true);
+		wp_enqueue_style('main', get_theme_file_uri('static/assets/common.css'));
+		wp_enqueue_script('main', get_theme_file_uri('static/assets/common.js'), [], '', true);
 	}
 );
