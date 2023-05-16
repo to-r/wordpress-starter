95fdf39d1ada4b0d9e1265b7b63c8350aef68974
Katsushi OUGI <ougi@to-r.net>
2023-05-16 20:23:52 +0900

    JS追加

--- wp-content/themes/my-theme/static/assets/js/common.js
+++ wp-content/themes/my-theme/static/assets/js/common.js
@@ -1,3 +1,42 @@
-(() => {
-  console.log("common.js");
+const MY_APP = {};
+
+MY_APP.namespace = (ns) => {
+  const parts = ns.split(".");
+  const parent = MY_APP;
+
+  if (parts[0] === "MY_APP") {
+    parts = parts.slice(1);
+  }
+
+  for (let i = 0; i < parent.length; i++) {
+    if (typeof parent[parts[i]] === "undefined") {
+      parent[parts[i]] = {};
+    }
+    parent = parent[parts[i]];
+  }
+
+  return parent;
+};
+
+MY_APP.Utils = (() => {
+  const getUrlParams = () => {
+    const params = {};
+    const search = location.search.substring(1);
+    const parameters = search.split("&");
+
+    if (!search) return {};
+
+    for (var i = 0; i < parameters.length; i++) {
+      const keyValue = parameters[i].split("=");
+      if (keyValue.length === 2) {
+        params[decodeURIComponent(keyValue[0])] = decodeURIComponent(
+          keyValue[1]
+        );
+      }
+    }
+
+    return params;
+  };
+
+  return { getUrlParams };
 })();
---
+++ wp-content/themes/my-theme/static/assets/js/modules/side_navigation.js
@@ -0,0 +1,5 @@
+MY_APP.SideNavigation = ((ns) => {
+  document.addEventListener("DOMContentLoaded", () => {
+    console.log("side navigation");
+  });
+})(MY_APP);
